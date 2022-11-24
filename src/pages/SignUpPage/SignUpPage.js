f4427133862c2cebb684d48bf22c9f0711b2d4a5
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2022-11-24 10:14:31 -0300

    feat: connected sign-up route

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,32 +1,163 @@
 import styled from "styled-components";
 import { MdArrowBackIos } from "react-icons/md";
 import Logo from "../../components/Logo";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function SignUpPage() {
+  const navigate = useNavigate();
+  const [disabledButton, setDisabledButton] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [signUpForm, setSignUpForm] = useState({
+    email: "",
+    cpf: "",
+    name: "",
+    surname: "",
+    birthDate: "",
+    telephone: "",
+    password: "",
+  });
+
+  function handleForm(event) {
+    const { name, value } = event.target;
+    if (name === "confirmPassword" && errorPassword === true) {
+      setErrorPassword(false);
+    }
+    setSignUpForm({ ...signUpForm, [name]: value });
+  }
+
+  function signUp(event) {
+    event.preventDefault();
+    setDisabledButton(true);
+
+    if (signUpForm.password === signUpForm.confirmPassword) {
+      const body = {
+        email: signUpForm.email,
+        cpf: signUpForm.cpf,
+        name: signUpForm.name,
+        surname: signUpForm.surname,
+        birthDate: signUpForm.birthDate,
+        telephone: signUpForm.telephone,
+        password: signUpForm.password,
+      };
+      axios
+        .post(`http://localhost:5000/sign-up`, body)
+        .then(() => navigate("/"))
+        .catch((error) => {
+          alert(error.response.data);
+          setDisabledButton(false);
+        });
+    } else {
+      setErrorPassword(true);
+      setDisabledButton(false);
+    }
+  }
+
   return (
     <PageContainer>
       <Logo />
       <BackButton>
         <MdArrowBackIos />
       </BackButton>
-      <Form>
+      <Form onSubmit={signUp}>
         <h1>Quero criar uma conta</h1>
         <Label>E-mail:</Label>
-        <Input></Input>
+        <Input
+          name="email"
+          value={signUpForm.email}
+          onChange={handleForm}
+          type="text"
+          placeholder="Digite seu e-mail"
+          disabled={disabledButton}
+          required
+        ></Input>
         <p>Dados Pessoais</p>
-        <Label>CPF:</Label>
-        <Input></Input>
+        <Label htmlFor="cpf">CPF:</Label>
+        <Input
+          name="cpf"
+          value={signUpForm.cpf}
+          onChange={handleForm}
+          type="text"
+          placeholder="___.___.___.-__"
+          minLength="11"
+          maxLength="11"
+          disabled={disabledButton}
+          required
+        ></Input>
         <Label>Nome:</Label>
-        <Input></Input>
+        <Input
+          name="name"
+          value={signUpForm.name}
+          onChange={handleForm}
+          type="text"
+          placeholder="Digite seu nome"
+          disabled={disabledButton}
+          required
+        ></Input>
         <Label>Sobrenome:</Label>
-        <Input></Input>
+        <Input
+          name="surname"
+          value={signUpForm.surname}
+          onChange={handleForm}
+          type="text"
+          placeholder="Digite seu sobrenome"
+          disabled={disabledButton}
+          required
+        ></Input>
         <Label>Data de nascimento:</Label>
-        <Input></Input>
-        <Label>Senha:</Label>
-        <Input></Input>
+        <Input
+          name="birthDate"
+          value={signUpForm.birthDate}
+          onChange={handleForm}
+          type="text"
+          placeholder="DD/MM/AAAA"
+          minLength="10"
+          maxLength="10"
+          disabled={disabledButton}
+          required
+        ></Input>
+        <Label>Telefone:</Label>
+        <Input
+          name="telephone"
+          value={signUpForm.telephone}
+          onChange={handleForm}
+          type="text"
+          placeholder="Digite seu telefone"
+          minLength="11"
+          maxLength="11"
+          disabled={disabledButton}
+          required
+        ></Input>
+        <Label>Senha (mínimo 6 caracteres):</Label>
+        <Input
+          name="password"
+          value={signUpForm.password}
+          onChange={handleForm}
+          type="password"
+          placeholder="Digite a senha"
+          minLength="6"
+          disabled={disabledButton}
+          required
+        ></Input>
         <Label>Repetir senha:</Label>
-        <Input></Input>
-        <Button>Criar conta</Button>
+        <InputConfirm
+          name="confirmPassword"
+          value={signUpForm.confirmPassword}
+          onChange={handleForm}
+          type="password"
+          placeholder="Confirme a senha"
+          minLength="6"
+          disabled={disabledButton}
+          errorPassword={errorPassword}
+          required
+        ></InputConfirm>
+        {errorPassword && (
+          <ErrorPassword>As senhas não conferem!</ErrorPassword>
+        )}
+        <Button type="submit" disabled={disabledButton}>
+          Criar conta
+        </Button>
       </Form>
     </PageContainer>
   );
@@ -87,6 +218,31 @@ const Input = styled.input`
   height: 38px;
   border-radius: 5px;
   margin: 5px 0;
+  border: none;
+  background-color: #ffffff;
+  padding: 15px;
+  font-family: "Comfortaa", cursive;
+  font-weight: 400;
+  font-size: 14px;
+  color: #000000;
+  opacity: ${(props) => (props.disabled ? "0.6" : "1")};
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px #ffffff inset !important;
+    -webkit-text-fill-color: #000000 !important;
+  }
+`;
+
+const InputConfirm = styled(Input)`
+  border: ${(props) => (props.errorPassword ? "3px solid red" : "none")};
+`;
+
+const ErrorPassword = styled.span`
+  font-family: "Comfortaa", cursive;
+  font-weight: 700;
+  font-size: 14px;
+  color: #ffffff;
+  text-align: center;
+  margin-bottom: 13px;
 `;
 
 const Button = styled.button`
@@ -100,4 +256,5 @@ const Button = styled.button`
   font-size: 16px;
   color: #ffffff;
   margin-top: 50px;
+  cursor: ${(props) => (props.disabled ? "cursor" : "pointer")};
 `;
