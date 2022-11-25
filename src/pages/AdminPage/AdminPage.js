d1a6c1705d4fe21d3ea28d4ecdb41345bdcc5ac6
erickssguerra <erickssguerra@gmail.com>
2022-11-24 23:20:27 -0300

    feat: selectedSeasons from ContainerSeasons + form mounting

--- src/pages/AdminPage/AdminPage.js
+++ src/pages/AdminPage/AdminPage.js
@@ -5,7 +5,7 @@ import ContainerSeasonsStyled from "./ContainerSeasons";
 import TextAreaDescription from "./TextAreaDescription";
 import InputTitle from "./InputTitle";
 import InputImage from "./InputImage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function AdminPage() {
   const [selectedState, setSelectedState] = useState("MA");
@@ -13,10 +13,50 @@ function AdminPage() {
   const [selectedTitle, setSelectedTitle] = useState("");
   const [selectedDescription, setSelectedDescription] = useState("");
   const [selectedImage, setSelectedImage] = useState("");
+  const [selectedSeasons, setSelectedSeasons] = useState({
+    altaTemporada: "",
+    baixaTemporada: "",
+  });
+
+  const form = {
+    admin: "erick1@gmail.com", //mudar depois que tiver o token
+    title: selectedTitle,
+    state: selectedState,
+    description: selectedDescription,
+    image: selectedImage,
+    tags: selectedCategories,
+    prices: {
+      altaTemporada: Number(
+        selectedSeasons.altaTemporada.replace(".", "").replace(",", "")
+      ),
+      baixaTemporada: Number(
+        selectedSeasons.baixaTemporada.replace(".", "").replace(",", "")
+      ),
+      cafe: 5000,
+      translado: 50000,
+    },
+    stock: {
+      natal: 10,
+      carnaval: 10,
+      reveillon: 10,
+      pascoa: 10,
+      baixaTemporada1: 20,
+      baixaTemporada2: 20,
+    },
+  };
+
+  function adicionar(event) {
+    event.preventDefault();
+    if (selectedCategories.length === 0) {
+      alert("Selecione pelo menos uma categoria!");
+      return;
+    }
+    console.log(form);
+  }
 
   // antes de dar o post, verificar pelo front se pelo menos uma categoria foi selecionada
   return (
-    <AdminPageStyle onSubmit="">
+    <AdminPageStyle onSubmit={adicionar}>
       <h1>Cadastro de produtos</h1>
       <h2>Selecione o Estado</h2>
       <ContainerStates setSelectedState={setSelectedState} />
@@ -36,11 +76,14 @@ function AdminPage() {
         selectedDescription={selectedDescription}
       />
       <h2>URL da imagem</h2>
-      <InputImage 
-      setSelectedImage={setSelectedImage}
-      selectedImage={selectedImage}
+      <InputImage
+        setSelectedImage={setSelectedImage}
+        selectedImage={selectedImage}
       />
-      <ContainerSeasonsStyled />
+      <ContainerSeasonsStyled
+        selectedSeasons={selectedSeasons}
+        setSelectedSeasons={setSelectedSeasons}
+      />
       <button type="submit">Adicionar</button>
     </AdminPageStyle>
   );
