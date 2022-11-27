f77bc6d3e2fd4bd8f6495a14ab72ebd5093a1dd6
erickssguerra <erickssguerra@gmail.com>
2022-11-27 00:20:49 -0300

    fix: TotalFooter fits the whole width

--- src/pages/ShoppingCartPage/TotalFooter.js
+++ src/pages/ShoppingCartPage/TotalFooter.js
@@ -3,31 +3,38 @@ import styled from "styled-components";
 export default function TotalFooter() {
   return (
     <TotalFooterStyle>
-      <h3>Total: R$ 3.500,00</h3>
-      <button>Continuar</button>
+      <div>
+        <h3>Total: R$ 3.500,00</h3>
+        <button>Continuar</button>
+      </div>
     </TotalFooterStyle>
   );
 }
 
 const TotalFooterStyle = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-  width: 390px;
+  justify-content: center;
+  width: 100%;
   background-color: white;
   height: 50px;
   bottom: 0;
   position: fixed;
-  h3 {
-    font-size: 16px;
-    font-weight: 700;
-  }
-  button {
-    width: 100px;
-    height: 35px;
-    background-color: #4ecb71;
-    color: white;
-    font-size: 14px;
+  div {
+    align-items: center;
+    padding: 10px;
+    display: flex;
+    width: 390px;
+    justify-content: space-between;
+    h3 {
+      font-size: 16px;
+      font-weight: 700;
+    }
+    button {
+      width: 100px;
+      height: 35px;
+      background-color: #4ecb71;
+      color: white;
+      font-size: 14px;
+    }
   }
 `;
