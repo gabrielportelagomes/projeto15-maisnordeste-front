8353653c47673d10f987257bb68d492a157b218c
erickssguerra <erickssguerra@gmail.com>
2022-11-28 16:40:06 -0300

    style: HomePage expanding cards

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import URL from "../../constants/url";
 import LoadingPage from "../../assets/styles/LoadingPage";
 import Header from "../../components/Header";
+import StateCard from "./StateCard";
 
 function HomePage() {
   const [states, setStates] = useState([]);
@@ -31,14 +31,10 @@ function HomePage() {
   return (
     <PageContainer>
       <Header />
-      <Title>Qual Nordeste você quer conhecer?</Title>
+      <Title>Qual Estado você quer conhecer?</Title>
       <StatesContainer>
         {states.map((state, id) => (
-          <Link key={id} to={`/estados/${state.state}`}>
-            <State key={id} stateImage={state.image} id={state.state}>
-              <p>{state.name}</p>
-            </State>
-          </Link>
+          <StateCard props={state} />
         ))}
       </StatesContainer>
     </PageContainer>
@@ -67,51 +63,12 @@ const Title = styled.h1`
 
 const StatesContainer = styled.div`
   width: 340px;
-  margin-top: 30px;
-  margin-bottom: 70px;
+  margin-top: 20px;
+  margin-bottom: 20px;
   overflow-y: auto;
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
   &::-webkit-scrollbar {
     display: none; /* Chrome */
   }
-`;
-
-const State = styled.div`
-  width: 340px;
-  height: 60px;
-  display: flex;
-  align-items: flex-end;
-  border-radius: 5px;
-  background-color: aliceblue;
-  background-image: url(${(props) => props.stateImage});
-  background-position: center;
-  background-size: cover;
-  cursor: pointer;
-  p {
-    font-family: "Comfortaa", cursive;
-    font-weight: 700;
-    font-size: 28px;
-    color: #ffffff;
-    margin: 5px;
-  }
-  &:hover {
-    height: 200px;
-    transition: 500ms;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  position: fixed;
-  bottom: 30px;
-`;
-
-const ActivitiesButton = styled.button`
-  width: 230px;
-  height: 35px;
-  background-color: #3003b2;
-  font-weight: 700;
-  font-size: 16px;
-  color: #ffffff;
-  cursor: pointer;
 `;
